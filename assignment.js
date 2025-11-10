920507bb90a9abf727c690e0c8fbd3940abe750f
snehaaadhikari <snehaadhikari78@gmail.com>
2025-11-10 09:26:59 +0545

    Update Versions

--- assignment.js
+++ assignment.js
@@ -14,3 +14,49 @@ function displaySum() {
   const displaySum = add();
   document.getElementById("text5").innerText = displaySum;
 }
+function Calculate1(){
+    let num1 = 50;
+    let num2 = 10;
+    return sum = num1 - num2;   
+}
+
+function display1()
+{
+    document.getElementById("text7").innerHTML = Calculate1();
+}
+
+function Calculate2(){
+    let num1 = 5;
+    let num2 = 3;
+    return sum = num1 * num2;   
+}
+
+function display2()
+{
+    document.getElementById("text8").innerHTML = Calculate2();
+}
+
+function Calculate3(){
+    let num1 = 15;
+    let num2 = 5;
+    return sum = num1 / num2;   
+}
+
+function display3()
+{
+    document.getElementById("text9").innerHTML = Calculate3();
+}
+function ControlFunc(){
+    let num1 = 2;
+    let num2 = 5;
+    if (num1 > num2) {
+        return epp = num1 / num2;
+    } else {
+        return epp = num2 * num1;
+    }
+}
+
+function display4()
+{
+    document.getElementById("text10").innerHTML = ControlFunc();
+}
